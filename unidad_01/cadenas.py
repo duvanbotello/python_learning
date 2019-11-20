b8f7f6c9f10cf37c76ccad42a7124985edd62b95
Duvan Botello <this.duvan@gmail.com>
2019-11-20 09:39:33 -0500

    Numericos y Cadenas

---
+++ unidad_01/cadenas.py
@@ -0,0 +1,5 @@
+nombre = "duvan"
+edad = 23
+altura = 1.76
+
+print("Nombre: %s Edad: %d Altura: %f" % (nombre,edad,altura))
---
+++ unidad_01/numericos.py
@@ -0,0 +1,6 @@
+n = 45
+print(n, type(n))
+
+f=4.57
+print(f, type(f))
+
